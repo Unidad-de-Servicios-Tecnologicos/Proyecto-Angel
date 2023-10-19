e09c4997f26b58f9cf986c88ce89888e6d33a370
Thomas <115964520+Tomassoft@users.noreply.github.com>
2023-10-19 10:44:18 -0500

    solucion de errores

--- app/src/main/java/com/example/angel/MainActivity.java
+++ app/src/main/java/com/example/angel/MainActivity.java
@@ -9,10 +9,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-<<<<<<< HEAD
         setContentView(R.layout.pagina_principal);
-=======
-        setContentView(R.layout.activity_pagina_inicio);
->>>>>>> master
+
     }
 }
