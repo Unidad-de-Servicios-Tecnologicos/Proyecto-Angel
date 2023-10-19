349f5744b97e5644a7cd958eb7421939b56514d5
Thomas <115964520+Tomassoft@users.noreply.github.com>
2023-10-19 09:25:52 -0500

    rama Thomas fusionada con master

--- app/src/main/java/com/example/angel/pagina2.java
+++ app/src/main/java/com/example/angel/pagina2.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class pagina2 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_pagina_inicio);
+        setContentView(R.layout.activity_pagina2);
     }
 }
